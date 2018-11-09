bc21b5933a5506410cf773006f235a360d5c0aff
Oren Powell <oap1@students.uwf.edu>
2018-11-09 10:53:44 -0600

    Revised to last approved code

--- Website/Sample.js
+++ Website/Sample.js
@@ -1,15 +1,17 @@
-
+// API Call to MySportsFeed.com
+/*
 $.ajax
 ({
   type: "GET",
-  url: https://api.mysportsfeeds.com/v1.0/pull/nfl/2017-regular/cumulative_player_stats.json?player=smith,
+  url: "https://api.mysportsfeeds.com/v1.0/pull/nfl/2017-regular/cumulative_player_stats.json",
   dataType: 'json',
   async: false,
   headers: {
-    "Authorization": "Basic " + btoa(389c9cb2-7e84-4cb4-a0ec-b2fbaa + ":" + Apr231991)
+    "Authorization": "Basic " + btoa("389c9cb2-7e84-4cb4-a0ec-b2fbaa:Apr231991")
   },
-  data: '{ "comment" }',
+  data: {  team:"miami-dolphins"  },
   success: function (){
     alert('Thanks for your comment!'); 
   }
 });
+*/
